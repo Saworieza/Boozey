eefb4d2ed4ad9a354cb3b27baa8be339ac50c09e
Sawo <saworiieza@gmail.com>
2016-04-26 10:40:52 +0000

    added a few stuff to products. throwing error on create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
- resources :products, only: [:index]
+ resources :products #, only: [:index]
  resource :cart, only: [:show]
  resources :order_items, only: [:create, :update, :destroy]
+ 
  root to: "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
